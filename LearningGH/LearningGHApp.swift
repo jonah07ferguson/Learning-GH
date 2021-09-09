b68b03ca044ac250624ebb1c61bd3ca55ff5122b
Jonah Ferguson <jonah07ferguson@gmail.com>
2021-09-09 11:04:06 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jonah Ferguson on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
